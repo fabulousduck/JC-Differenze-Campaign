687804cb0eb7a18c3836439e7b4b522630d2bff8
18020 <18020@users.noreply.github.com>
2016-04-21 20:18:00 +0200

    Added files via upload

---
+++ tienplus.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/style.css" type="text/css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link href='https://fonts.googleapis.com/css?family=PT+Sans:400,400italic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/animations.css" type="text/css" />
+    <title>Wat is jouw talent?</title>
+</head>
+<body>
+    
+    <header id="header">
+        <div class="logo"></div>
+        
+
+    
+        <div id="slide-window">
+  
+                <ol id="slides" start="1">
+                        <li class="slide color-1 alive" style="background-image:url(images/social.jpg);"></li>
+                        <li class="slide color-2" style="background-image:url(images/maze2.jpg);" ></li>
+                        <li class="slide color-3" style="background-image:url(images/schakel.jpg);"></li>
+                </ol>
+    
+        </div>
+        
+        <div id="wrapperHeader">
+            <div class="elements">
+            <img src="images/logo.png"></img>
+                <h2>Wat is jouw leeftijd?</h2>
+                    <div id="buttons">
+                        <div class="ageButtons tenPlusButton"><a><h1>10+</h1></a></div>
+                        <div class="ageButtons sixteenPlusButton"><a><h1>16+</h1></a></div>
+                        <div class="clear"></div>
+                    </div>
+            </div>
+        <div class="clear"></div>
+        </div>    
+        
+        
+        
+    </header>
+    
+    <nav class="navigation">
+        <div class="navigationWrapper">
+            <div class="logo"><img src="images/icon.png"></img></div>
+            <div class="menu">
+                <ul>
+                    <li><a href="">App</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+                
+            </div>
+        </div>
+    </nav>
+    
+    <div id="container">
+        <div class="contentContainer">      
+            <div class="leftContent">a</div>
+            <div class="rightContent"><p class="tienplus_tekst">Als kind wordt je vaak verteld over wat je niet kan.</p></div>
+            
+            <div class="leftContent"><p class="tienplus_tekst">Meestal moet je luisteren naar deze mensen, maar soms moet je ze ook gewoon even negeren.</p></div>
+            <div class="rightContent">a</div>
+            
+            <div class="leftContent">a</div>
+            <div class="rightContent"><p class="tienplus_tekst">Negeer op dit moment even alles op social media en focus je op jezelf.</p></div>
+            
+            <div class="leftContent"><p class="tienplus_tekst">Maak je eigen weg naar de top.</p></div>
+            <div class="rightContent">a</div>
+            
+            <div class="leftContent">a</div>
+            <div class="rightContent"><p class="tienplus_tekst">zet je eigen puzzelstukjes in elkaar en denk goed na over wat jij leiuk vindt.</p></div>
+            
+            <div class="leftContent"><p class="tienplus_tekst">Misschien kan je het niet alleen en heb je een beetje hulp nodig.</p>c</div>
+            <div class="rightContent">a</div>
+            
+            <div class="leftContent">a</div>
+            <div class="rightContent"><p class="tienplus_tekst">Met een beetje hulp van ons kunnen we er samen achter komen wat jouw talent is.</p></div>
+            
+        </div> 
+    </div> 
+    
+        
+    <div class="clear"><!--Sick clearfix voor de floats gozaah--></div>
+
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+    <script type="text/javascript" src="logic/js/index.js"></script>
+    <script type="text/javascript" src="logic/js/slideshow.js"></script>
+    <script type="text/javascript" src="logic/js/stellar.js"></script>
+    <script type="text/javascript" src="logic/js/navigation.js"></script>
+</body>
+</html>
