4808af9b2518acd8b712eeb788474f7c7fbe2377
18020 <18020@users.noreply.github.com>
2016-04-25 11:30:20 +0200

    Update tienplus.php

--- web/tienplus.php
+++ web/tienplus.php
@@ -31,7 +31,7 @@
             <img src="images/logo.png"></img>
                 <h2>Wat is jouw leeftijd?</h2>
                     <div id="buttons">
-                        <div class="ageButtons tenPlusButton"><a><h1>10+</h1></a></div>
+                        <div class="ageButtons tenPlusButton"><a href="tienplus.php"><h1>10+</h1></a></div>
                         <div class="ageButtons sixteenPlusButton"><a><h1>16+</h1></a></div>
                         <div class="clear"></div>
                     </div>
@@ -61,7 +61,7 @@
             <div class="leftContent als_kind"></div>
             <div class="rightContent"><p class="tienplus_tekst">Als kind wordt je vaak verteld over wat je niet kan.</p></div>
             
-            <div class="leftContent"><p class="tienplus_tekst">Meestal moet je luisteren naar deze mensen, maar soms moet je ze ook gewoon even negeren.</p><p class="tienplus_hand"><i class="fa fa-hand-paper-o" aria-hidden="true"></i></p></div>
+            <div class="leftContent"><p class="tienplus_tekst tekst_meestal_moet_je_luisteren">Meestal moet je luisteren naar deze mensen, maar soms moet je ze ook gewoon even negeren.</p><p class="tienplus_hand"><i class="fa fa-hand-paper-o" aria-hidden="true"></i></p></div>
             <div class="rightContent meestal_moet_je_luisteren"></div>
             
             <div class="leftContent negeer_op_dit_moment"></div>
@@ -73,7 +73,7 @@
             <div class="leftContent zet_je_eigen_puzzelstukjes"></div>
             <div class="rightContent"><p class="tienplus_tekst">zet je eigen puzzelstukjes in elkaar en denk goed na over wat jij leiuk vindt.</p></div>
             
-            <div class="leftContent"><p class="tienplus_tekst">Misschien kan je het niet alleen en heb je een beetje hulp nodig.</p>c</div>
+            <div class="leftContent"><p class="tienplus_tekst">Misschien kan je het niet alleen en heb je een beetje hulp nodig.</p></div>
             <div class="rightContent misschien_kan_je_het_niet_alleen"></div>
             
             <div class="leftContent met_een_beetje_hulp"></div>
@@ -81,9 +81,9 @@
             
         </div> 
         
-         <div id="buttoons">
-                    <a class="left" href="http://ma.cruxpanel.nl/workspace_3/app">QUIZ!</a>
-                    <a class="right" href="http://jc-differenze.nl">Jc Differenze</a>
+         <div id="buttoons" class="tienplus_buttons">
+                    <a class="left"  target="_blank" href="http://ma.cruxpanel.nl/workspace_3/app">QUIZ!</a>
+                    <a class="right" target="_blank" href="http://jc-differenze.nl">Jc Differenze</a>
          </div>
         
     </div> 
@@ -91,8 +91,8 @@
         
     <div class="clear"><!--Sick clearfix voor de floats gozaah--></div>
 
-<footer class="footer">
-        <p>&copy; All rights reserved | Project by <a href="http://ma-web.nl">Mediacollege Amsterdam</a></p>
+<footer class="footer tienplus">
+        <p>&copy; All rights reserved | Project by <a target="_blank" href="http://ma-web.nl">Mediacollege Amsterdam</a></p>
     </footer>
     
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
